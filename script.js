54fa3c43c7838ce88a28ecb57f17070d36445067
Subhajit Mandal <108950928+Subhajit-mn@users.noreply.github.com>
2023-09-18 23:36:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
-	let y = navigator.appName;
-	let z = navigator.appVersion;
-	document.getElementById("browser-info").innerHtML = "You are using " + y + " version " + z.
+	let browserName = navigator.appName;
+	let version = navigator.appVersion;
+	document.getElementById("browser-info").innerHtML = "You are using " + browserName + " version " + version.
 		
 
 	
