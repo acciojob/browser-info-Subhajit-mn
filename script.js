8100972a591b3d8a1f34fdcebf460824d340bc2b
Subhajit Mandal <108950928+Subhajit-mn@users.noreply.github.com>
2023-09-18 23:33:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 //your JS code here. If required.
-function browsweDetect{
-	let x = navigator.userAgent;
 	let y = navigator.appName;
 	let z = navigator.appVersion;
-	document.getElementById("browser-info") = "You are using " + y + " version " + z.
-}
+	document.getElementById("browser-info").innerHtML = "You are using " + y + " version " + z.
+		
 
 	
